a46c274b5da4e32f5627ba43e6e75c5f0ef21309
fengjianchao <jcfeng2013@gmail.com>
2019-08-12 23:04:37 +0800

    init add

---
+++ start.py
@@ -0,0 +1,42 @@
+import json
+import time
+import datetime
+import requests
+from client import HeyBoxClient
+
+
+def send_result(content, skey):
+    url = 'https://sc.ftqq.com/%s.send' % skey
+    data = {
+        'text': '小黑盒脚本',
+        'desp': content
+    }
+    resp = requests.get(url, data)
+    print('执行结束', resp.status_code, resp.json())
+
+
+def load_settings():
+    with open('config.json', 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+
+def daily_task():
+    settings = load_settings()
+    heybox = HeyBoxClient(settings['account'])
+
+    heybox.auto()  # 自动完成每日任务，自动动态点赞
+    detail = heybox.get_task_detail()  # 获取任务详情
+    my_data = heybox.get_my_data()
+
+    result = ', '.join((detail, my_data))
+    send_result(result, settings['ftqqskey'])
+
+
+if __name__ == '__main__':
+    while True:
+        now = datetime.datetime.now()
+        if now.hour == 8 and now.minute == 30:
+            daily_task()
+        else:
+            print(now)
+            time.sleep(60)
